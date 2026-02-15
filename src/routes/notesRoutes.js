b979267a29c2f633a0844253fdcb686b940bbe0a
Nikita Shulga <nikitashulga1996@gmail.com>
2026-02-15 02:22:35 +0200

    Add auth with sessions and private notes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -16,7 +16,12 @@ import {
     updateNoteSchema,
 } from '../validations/notesValidation.js';
 
+import { authenticate } from '../middlewares/authenticate.js';
+
 const router = Router();
+
+// ВСІ нотатки доступні тільки після авторизації
+router.use(authenticate);
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
